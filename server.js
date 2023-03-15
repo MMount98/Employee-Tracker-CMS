d3e9d0ee8592d50a9b18c6afd484faa9d845a015
Michael Mount <mount.photos@gmail.com>
2023-03-14 18:00:06 -0600

    User Input now will return the deparment and employee table if prompted

--- server.js
+++ server.js
@@ -12,37 +12,45 @@ const db = mysql.createConnection(
   console.log(`Connected to Employee Database!`)
 );
 
-// inquirer
-//   .prompt([
-//     {
-//       type: "list",
-//       name: "selection",
-//       message: "What would you like to do?",
-//       choices: [
-//         "View All Departments",
-//         "View All Employees",
-//         "Add a Department",
-//         "Add a Role",
-//         "Add an Employee",
-//         "Update an Employee role",
-//         "Quit",
-//       ],
-//     },
-//   ])
-//   .then((data) => {
-//     if (data === "View All Departments")
-//       db.query("SELECT * FROM departments", function (err, results) {
-//         return results;
-//       });
-//   })
-//   .catch((error) => {
-//     if (error.isTtyError) {
-//       console.log("Coudnlt Render Prompt");
-//     } else {
-//       Console.log("Error with inquire, check if update is needed");
-//     }
-//   });
-
-db.query("SELECT * FROM departments", function (err, results) {
-  console.table(results);
-});
+inquirer
+  .prompt([
+    {
+      type: "list",
+      name: "selection",
+      message: "What would you like to do?",
+      choices: [
+        "View All Departments",
+        "View All Employees",
+        "Add a Department",
+        "Add a Role",
+        "Add an Employee",
+        "Update an Employee role",
+        "Quit",
+      ],
+    },
+  ])
+  .then((data) => {
+    switch (data.selection) {
+      case "View All Departments":
+        db.query("SELECT * FROM departments", function (err, results) {
+          console.table(results);
+        });
+        break;
+      case "View All Employees":
+        db.query("SELECT * FROM employee", function (err, results) {
+          console.table(results);
+        });
+        break;
+      case "Add a Department":
+        break;
+      default:
+        throw Error("Failed to pass shape value to Constructor");
+    }
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log("Coudnlt Render Prompt");
+    } else {
+      console.log("Error with inquire, check if update is needed");
+    }
+  });
