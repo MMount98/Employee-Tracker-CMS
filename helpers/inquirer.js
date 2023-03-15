971f41ef2e48b18bf344e65b2c437fa5054f2b39
Michael Mount <mount.photos@gmail.com>
2023-03-14 22:58:13 -0600

    built add role function

---
+++ helpers/inquirer.js
@@ -0,0 +1,77 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const table = require("console.table");
+const addRole = require("./add-role.js");
+
+const db = mysql.createConnection(
+  {
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "employee_db",
+  },
+  console.log(`Connected to Employee Database!`)
+);
+
+const initial = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "selection",
+        message: "What would you like to do?",
+        choices: [
+          "View All Departments",
+          "View All Employees",
+          "Veiw All Roles",
+          "Add a Department",
+          "Add a Role",
+          "Add an Employee",
+          "Update an Employee role",
+          "Quit",
+        ],
+      },
+    ])
+    .then((data) => {
+      switch (data.selection) {
+        case "View All Departments":
+          db.query("SELECT * FROM departments", function (err, results) {
+            console.table(results);
+            initial();
+          });
+          break;
+        case "View All Employees":
+          db.query(
+            "SELECT first_name, last_name, title, salary FROM employee JOIN role ON employee.role_id = role.id ",
+            function (err, results) {
+              console.table(results);
+              initial();
+            }
+          );
+          break;
+        case "Veiw All Roles":
+          db.query(
+            "SELECT title, name AS department, salary FROM role JOIN departments ON role.department_id = departments.id;",
+            function (err, results) {
+              console.table(results);
+              initial();
+            }
+          );
+          break;
+        case "Add a Role":
+          return addRole();
+          break;
+        default:
+          throw Error("Failed to pass shape value to Constructor");
+      }
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        console.log("Coudnlt Render Prompt");
+      } else {
+        console.log("Error with inquire, check if update is needed");
+      }
+    });
+};
+
+module.exports = initial;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 //Modules
-const inquirer = require("inquirer");
 const mysql = require("mysql2");
-const table = require("console.table");
+
+//Helpers
+const initial = require("./helpers/inquirer.js");
+
+//
 const db = mysql.createConnection(
   {
     host: "127.0.0.1",
@@ -12,45 +15,4 @@ const db = mysql.createConnection(
   console.log(`Connected to Employee Database!`)
 );
 
-inquirer
-  .prompt([
-    {
-      type: "list",
-      name: "selection",
-      message: "What would you like to do?",
-      choices: [
-        "View All Departments",
-        "View All Employees",
-        "Add a Department",
-        "Add a Role",
-        "Add an Employee",
-        "Update an Employee role",
-        "Quit",
-      ],
-    },
-  ])
-  .then((data) => {
-    switch (data.selection) {
-      case "View All Departments":
-        db.query("SELECT * FROM departments", function (err, results) {
-          console.table(results);
-        });
-        break;
-      case "View All Employees":
-        db.query("SELECT * FROM employee", function (err, results) {
-          console.table(results);
-        });
-        break;
-      case "Add a Department":
-        break;
-      default:
-        throw Error("Failed to pass shape value to Constructor");
-    }
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      console.log("Coudnlt Render Prompt");
-    } else {
-      console.log("Error with inquire, check if update is needed");
-    }
-  });
+initial();
